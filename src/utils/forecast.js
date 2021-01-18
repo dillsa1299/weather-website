3101f443847dd5c7f2b4180567275843f94f367f
Dylan Irwin <dylanirwin.ncl@gmail.com>
2021-01-18 13:34:32 +0000

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=5ffde0f24ca31868c7364457158b72b9&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({ url, json: true}, (error, {body} = {}) => {
+        if (error) {
+            callback('Unable to connect to weather services!', undefined)
+        } else if (body.error) {
+            callback('Unable to find weather location.', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + 
+            ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+        }
+    })
+}
+
+module.exports = forecast
