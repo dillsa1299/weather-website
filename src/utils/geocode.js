3bd0847074c535a41567b93cb8344384d97d5c63
Dylan Irwin <77620609+dillsa1299@users.noreply.github.com>
2024-02-15 19:01:45 +0000

    Update geocode.js
    Redacting API key

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('postman-request')
 
 const geocode = (address, callback) => {
-    const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZGlsbHNhMTI5OSIsImEiOiJja2swNXp2NnIwZWMyMm9wYzhtYXFucTFyIn0.kRmxyiLYQ2HjD1NdzeXB9g&limit=1'
+    const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=XXX'
 
     request({ url, json: true}, (error, {body} = {}) => {
         if (error) {
